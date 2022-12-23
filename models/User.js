a2c3fe0551d90e091cc15c296dfd322af5f7c95e
Roger Kinney <rogerkinney@Rogers-MacBook-Air.local>
2022-12-22 21:29:08 -0800

    https://github.com/cam1024/Botany-plants-lately

--- models/User.js
+++ models/User.js
@@ -5,6 +5,9 @@ const sequelize = require('../config/connection');
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
+  } catch (error) {
+    console.error(error);
+    return false;
   }
 }
 
